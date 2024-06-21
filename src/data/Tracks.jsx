01beaa5c41e256eb8689b60cb1810d76960979c9
Gagannagu <142592216+Gagannagu@users.noreply.github.com>
2024-06-21 21:18:08 +0530

    hello

---
+++ src/data/Tracks.jsx
@@ -0,0 +1,23 @@
+const tracks = [
+    {
+      id: 1,
+      title: "Song 1",
+      artist: "Artist 1",
+      src: "/path/to/song1.mp3",
+    },
+    {
+      id: 2,
+      title: "Song 2",
+      artist: "Artist 2",
+      src: "/path/to/song2.mp3",
+    },
+    {
+      id: 3,
+      title: "Song 3",
+      artist: "Artist 3",
+      src: "/path/to/song3.mp3",
+    },
+  ];
+  
+  export default tracks;
+  
